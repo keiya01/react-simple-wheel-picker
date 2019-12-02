8cfe4f93aaa8feeb3f037dd10d2332be40a99f7a
keiya01 <keiya.s.0210@gmail.com>
2019-12-02 14:03:32 +0900

    Fix storybook and storyshots for WheelPicker component

--- stories/WheelPicker.stories.tsx
+++ stories/WheelPicker.stories.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 import Component from "../src/components/WheelPicker";
-import { pickerData } from "../src/constants/mockData/pickerDate";
+import { pickerData } from "../src/constants/mockData/pickerData";
 
 export default {
   title: "components"
 };
 
-export const WheelPicker: React.FC = () => <Component data={pickerData} />;
+const onChange = (target: Element): void => {
+  console.log(target);
+};
+
+export const WheelPicker: React.FC = () => (
+  <Component data={pickerData} onChange={onChange} width={100} height={100} />
+);
