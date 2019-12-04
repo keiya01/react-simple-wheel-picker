24bd56711567228e2493c359a5615e764aceb0df
keiya01 <keiya.s.0210@gmail.com>
2019-12-04 19:33:26 +0900

    Update pickerData

--- src/constants/mockData/pickerData.ts
+++ src/constants/mockData/pickerData.ts
@@ -1,6 +1,28 @@
 import { PickerData } from "@/components/WheelPicker";
 
-export const pickerData: PickerData[] = [...Array(30)].map((_, i) => ({
-  id: i.toString(),
-  value: i
-}));
+export const pickerData: PickerData[] = [
+  {
+    id: "0",
+    value: "React"
+  },
+  {
+    id: "1",
+    value: "Angular"
+  },
+  {
+    id: "2",
+    value: "Vue"
+  },
+  {
+    id: "3",
+    value: "TypeScript"
+  },
+  {
+    id: "4",
+    value: "JavaScript"
+  },
+  {
+    id: "5",
+    value: "ReactNative"
+  }
+];
