ee9095029ab86efd884ccdd6d29b42f7d979be9c
keiya01 <keiya.s.0210@gmail.com>
2019-12-06 13:41:20 +0900

    Add scroll animation

--- src/hooks/useScrollAnimation.ts
+++ src/hooks/useScrollAnimation.ts
@@ -2,6 +2,37 @@ import { RefObject, useRef } from "react";
 import { PickerItemRef } from "@/types/pickerItemRef";
 import { PickerData } from "@/components/WheelPicker";
 
+const easeOutCubic = (t: number, b: number, c: number, d: number) => {
+  t /= d;
+  t--;
+  return c * (t * t * t + 1) + b;
+};
+
+const setScrollAnimation = (
+  root: HTMLUListElement,
+  currentPosition: number,
+  changingValue: number
+) => {
+  let start = 1;
+  let isStop = false;
+  const animation = () => {
+    if (isStop) return;
+    const offset = easeOutCubic(
+      start / 100,
+      currentPosition,
+      changingValue,
+      0.1
+    );
+    requestAnimationFrame(animation);
+    root.scrollTo(0, offset);
+    const target = currentPosition + changingValue;
+    start += 1;
+    isStop = offset === target;
+  };
+
+  return animation;
+};
+
 const useScrollAnimation = (
   root: RefObject<HTMLUListElement>,
   refs: PickerItemRef
@@ -20,9 +51,13 @@ const useScrollAnimation = (
     if (_root && basicElm && currentElm) {
       timer.current = setTimeout(() => {
         const basicOffsetTop = basicElm.offsetTop;
-        const currentOffsetTop = currentElm.offsetTop - basicOffsetTop;
-        const targetTop = (basicOffsetTop * currentOffsetTop) / basicOffsetTop;
-        _root.scrollTo(0, targetTop);
+        const targetOffsetTop = currentElm.offsetTop - basicOffsetTop;
+        const animation = setScrollAnimation(
+          _root,
+          _root.scrollTop,
+          targetOffsetTop - _root.scrollTop
+        );
+        requestAnimationFrame(animation);
       }, 500);
     }
   };
