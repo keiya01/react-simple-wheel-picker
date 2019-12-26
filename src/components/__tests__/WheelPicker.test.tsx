f509c474aba97064b281d79278e5e15b3300a262
keiya01 <keiya.s.0210@gmail.com>
2019-12-26 18:52:52 +0900

    Add test for tab and shift key operation

--- src/components/__tests__/WheelPicker.test.tsx
+++ src/components/__tests__/WheelPicker.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import WheelPicker, { WheelPickerRef } from "../WheelPicker";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+import { pickerData } from "../../constants/mockData/pickerData";
+import { setScrollAnimation } from "../../hooks/useScrollAnimation";
 
 declare global {
   interface Window {
@@ -23,6 +25,8 @@ jest.mock("react", () => ({
     }
   })
 }));
+
+jest.mock("../../hooks/useScrollAnimation");
 
 describe("WheelPicker", () => {
   describe("Set padding top to WheelPicker", () => {
@@ -111,6 +115,128 @@ describe("WheelPicker", () => {
       }
 
       expect(document.activeElement).not.toEqual(pickerList);
+    });
+  });
+
+  describe("Keyboard Operation", () => {
+    let scrollTop = 0;
+    beforeEach(() => {
+      jest
+        .spyOn(window, "requestAnimationFrame")
+        .mockImplementation((cb: any) => cb());
+
+      const _setScrollAnimation = setScrollAnimation as any;
+      _setScrollAnimation.mockImplementation(
+        (_: HTMLElement, __: number, itemHeight: number) => {
+          return () => {
+            scrollTop += itemHeight;
+          };
+        }
+      );
+    });
+
+    afterEach(() => {
+      scrollTop = 0;
+    });
+
+    it("should be moved to next option position when tab key was pressed", () => {
+      const { getByTestId } = render(
+        <WheelPicker
+          data={pickerData}
+          selectedID="1"
+          onChange={() => {}} // eslint-disable-line no-empty-function
+          height={100}
+          itemHeight={100}
+        />
+      );
+
+      const listbox = getByTestId("picker-list");
+
+      fireEvent.keyDown(listbox, {
+        keyCode: 9
+      });
+
+      expect(scrollTop).toEqual(100);
+    });
+
+    it("should be moved to previous option position when tab + shift key was pressed", () => {
+      const { getByTestId } = render(
+        <WheelPicker
+          data={pickerData}
+          selectedID="1"
+          onChange={() => {}} // eslint-disable-line no-empty-function
+          height={100}
+          itemHeight={100}
+        />
+      );
+
+      const listbox = getByTestId("picker-list");
+
+      fireEvent.keyDown(listbox, {
+        keyCode: 16
+      });
+
+      fireEvent.keyDown(listbox, {
+        keyCode: 9
+      });
+
+      expect(scrollTop).toEqual(-100);
+    });
+
+    it("should be moved to next option position when tab key was pressed before shift key was pressed", () => {
+      const { getByTestId } = render(
+        <WheelPicker
+          data={pickerData}
+          selectedID="1"
+          onChange={() => {}} // eslint-disable-line no-empty-function
+          height={100}
+          itemHeight={100}
+        />
+      );
+
+      const listbox = getByTestId("picker-list");
+
+      fireEvent.keyDown(listbox, {
+        keyCode: 16
+      });
+
+      fireEvent.keyUp(listbox, {
+        keyCode: 16
+      });
+
+      fireEvent.keyDown(listbox, {
+        keyCode: 9
+      });
+
+      expect(scrollTop).toEqual(100);
+    });
+
+    it("should be moved to next option position when shift key was released before tab key was pressed", () => {
+      const { getByTestId } = render(
+        <WheelPicker
+          data={pickerData}
+          selectedID="1"
+          onChange={() => {}} // eslint-disable-line no-empty-function
+          height={100}
+          itemHeight={100}
+        />
+      );
+
+      const listbox = getByTestId("picker-list");
+
+      fireEvent.keyDown(listbox, {
+        keyCode: 16
+      });
+
+      fireEvent.keyUp(listbox, {
+        keyCode: 16
+      });
+
+      fireEvent.keyDown(listbox, {
+        keyCode: 9
+      });
+
+      expect(scrollTop).toEqual(100);
     });
   });
 });
